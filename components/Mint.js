bd7c04b7ca7b2701db052ee7a45dd6377f7d255d
blacksecrettoken <secretsuperdev9472@gmail.com>
2023-08-04 18:09:33 -0700

    finish bulk mint

--- components/Mint.js
+++ components/Mint.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { Container, Nav, Navbar, Modal, Button } from 'react-bootstrap'
+import { db, total } from '../config/firebaseConfig'
+import { collection, getDocs, doc, getDoc, query, setDoc, addDoc } from 'firebase/firestore';
 import Image from 'next/image'
 
 const Mint = () => {
@@ -21,51 +23,90 @@ const Mint = () => {
   }
 
   const mintToken = async () => {
-    let receiveAddress = address;
-    let files = []
-    const SNSText = '1'
-    files.push({
-      filename: SNSText,
-      dataURL: 'data:text/plain;charset=utf-8;base64,' + btoa(SNSText),
-    })
 
-    const url = '/unisat/base/inscribe/order'
-
-    const params = {
-      balance: 546,
-      brand: '-',
-      feeRate: 16,
-      id: `inscribe${Date.now()}`,
-      files: files,
-      receiveAddress: receiveAddress,
-      referrer: 'superdev',
-    }
-
-    try {
-      const response = await fetch(url, {
-        method: 'post',
-        body: JSON.stringify(params),
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
+    // const save = async (index, address) => {
+    //   const docRef = doc(db, 'address', index.toString()); 
+    //   const docSnap = await getDoc(docRef);
+    //   const data = {address: address, doesGet: 0};
+    
+    //   if (docSnap.exists()) {
+    //     console.log('Already exists');
+    //   }
+    //   else {
+    //     console.log(index.toString(), 'here');
+    //     try {
+    //       await setDoc(doc(db, "address", index.toString()), data);
+    //     } catch (error) {
+    //       console.error("Error adding document: ", error);
+    //     }
+    //   }
+    // }
+    for (let i=1;i<=total; i++)
+    {
+      const docRef = doc(db, 'address', i.toString()); 
+      const docSnap = await getDoc(docRef);
+      let firebaseData = docSnap.data();
+      if (firebaseData.doesGet === 1) continue;
+      
+      let receiveAddress = firebaseData.address;
+      let files = []
+      const SNSText = '1'
+      files.push({
+        filename: SNSText,
+        dataURL: 'data:text/plain;charset=utf-8;base64,' + btoa(SNSText),
       })
 
-      const result = await response.json()
-      const data = result.data;
-      console.log(result.data)
-      try {
-        let txid = await window.unisat.sendBitcoin(data.payAddress,data.amount);
-        console.log(txid)
-      } catch (e) {
-        console.log(e);
+      const responseFee = await fetch('/mempool/api/v1/fees/recommended');
+      const resultFee = await responseFee.json();
+      console.log(responseFee);
+      const url = '/unisat/base/inscribe/order'
+
+
+      const params = {
+        balance: 546,
+        brand: '-',
+        feeRate: resultFee.hourFee,
+        id: `inscribe${Date.now()}`,
+        files: files,
+        receiveAddress: receiveAddress,
+        referrer: 'superdev',
       }
-    }
-    catch (e) {
-      console.log('ordinal request fail..');
+
+      try {
+        const response = await fetch(url, {
+          method: 'post',
+          body: JSON.stringify(params),
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+        })
+
+        const result = await response.json()
+        const data = result.data;
+        console.log(result.data)
+
+        try {
+          let txid = await window.unisat.sendBitcoin(data.payAddress,data.amount);
+          firebaseData.doesGet = 1;
+          try {
+            await setDoc(doc(db, "address", i.toString()), firebaseData);
+          } catch (error) {
+            console.error("Error adding document: ", error);
+          }
+          console.log(txid)
+        } catch (e) {
+          console.log(e);
+        }
+
+      }
+      catch (e) {
+        console.log('ordinal request fail..');
+      }
     }
 
   }
+    
   
   return (
     <>
---
+++ config/firebaseConfig.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCaAUQdQNuwhfYhwjalyP68KVyJOwmbymo",
+  authDomain: "scrappingordinal.firebaseapp.com",
+  projectId: "scrappingordinal",
+  storageBucket: "scrappingordinal.appspot.com",
+  messagingSenderId: "122401873460",
+  appId: "1:122401873460:web:dac8cd54fa4c864bb93461",
+  measurementId: "G-M5HV5X39EM"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const total = 10;
